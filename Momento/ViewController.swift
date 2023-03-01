433ddf56a752bc85142e65ada76d214a16203758
niyatiprabhu <niyatiprabhu@utexas.edu>
2023-03-01 00:02:10 -0600

    adding initial project files

---
+++ Momento/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Momento
+//
+//  Created by Niyati Prabhu on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
