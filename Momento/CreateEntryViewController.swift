c585d8be92c9841a060789eaf89d9f7d8bab2526
Hannah Clark <hannahlclarkk@utexas.edu>
2023-03-05 16:42:46 -0600

    finished CreateEntryViewController- added color well so user can pick background color

--- Momento/CreateEntryViewController.swift
+++ Momento/CreateEntryViewController.swift
@@ -2,55 +2,29 @@
 //  CreateEntryViewController.swift
 //  Momento
 //
-//  Created by jasmine wang on 3/5/23.
-//
 
 import UIKit
 
 class CreateEntryViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    var selectedColor = UIColor.white
+    
     @IBOutlet weak var textResponseField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        textResponseField.borderStyle = .roundedRect
-        textResponseField.textAlignment = .left
-        textResponseField.contentVerticalAlignment = .top
-    }
+    @IBOutlet weak var colorWell: UIColorWell!
     
     @IBAction func uploadPhotoPressed(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
-        present(imagePicker, animated: true, completion: nil)
+        present(imagePicker, animated: true)
     }
-
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
-                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
-            }
-            imageView.image = image
-            dismiss(animated:true, completion: nil)
-        }
     
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            dismiss(animated: true, completion: nil)
-        }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func backBtnPressed(_ sender: Any) {
         self.dismiss(animated: false)
     }
-    
+     
+    // *** ACCCESS SELECTED BACKGROUND COLOR WITH "selectedColor" ***
     @IBAction func onPostPressed(_ sender: Any) {
         let calendar = Calendar.current
         let today = calendar.dateComponents([.year, .month, .day], from: Date.now)
@@ -60,4 +34,37 @@ class CreateEntryViewController: UIViewController, UIImagePickerControllerDelega
         // display the new post
         performSegue(withIdentifier: "EntryJustPosted", sender: self)
     }
+    
+    // set up textField attribues and color well
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textResponseField.borderStyle = .roundedRect
+        textResponseField.textAlignment = .left
+        textResponseField.contentVerticalAlignment = .top
+        setupColorWell()
+    }
+    
+    // image picker pop up- allow a user to select an image from camera roll
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected an image but didn't find one")
+        }
+        imageView.image = image
+        dismiss(animated:true)
+    }
+    
+    // sets up the color well/ makes connection when value changes
+    func setupColorWell() {
+        self.view.addSubview(colorWell)
+        colorWell.title = "Background Color"
+        colorWell.addTarget(self, action: #selector(colorWellChanged(_:)), for: .valueChanged)
+    }
+    
+    // if color well value changed, change the background
+    @objc func colorWellChanged(_ sender: Any) {
+        self.view.backgroundColor = colorWell.selectedColor
+        selectedColor = colorWell.selectedColor!
+    }
+    
+    
 }
