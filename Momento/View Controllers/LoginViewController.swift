74d4a23b9a8bbdc7ef5badd9d5c44fab28f9fbfe
jasmineywang <jasminewang@utexas.edu>
2023-03-07 13:30:31 -0600

    logout to login segue

--- Momento/View Controllers/LoginViewController.swift
+++ Momento/View Controllers/LoginViewController.swift
@@ -29,17 +29,7 @@ class LoginViewController: UIViewController {
         }
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     @IBAction func loginButtonPressed(_ sender: Any) {
         Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!) {
             authResult, error in if let error = error as NSError? {
@@ -47,10 +37,6 @@ class LoginViewController: UIViewController {
             }
         }
     }
-    
-    @IBAction func signupButtonPressed(_ sender: Any) {
-    }
-    
     
 
 }
--- Momento/View Controllers/SettingsViewController.swift
+++ Momento/View Controllers/SettingsViewController.swift
@@ -17,19 +17,11 @@ class SettingsViewController: UIViewController {
     }
     
 
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     @IBAction func logoutBtnPressed(_ sender: Any) {
         do {
             try Auth.auth().signOut()
+            performSegue(withIdentifier: "LogoutSegue", sender: nil)
         } catch {
             print("logout error")
         }
